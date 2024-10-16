1b5aff6e9dc387f2fdf7e5067f33929229626203
Benito <ntambube@gmail.com>
2024-10-16 21:52:25 +0100

    Page Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,11 +6,42 @@ import NavBar from "../components/NavBar";
 
 function Contact() {
     return (
-        <header>
-            <NavBar />
-        </header>
+        <>
+            <header>
+                <NavBar />
+            </header>
+            <Main />
+        </>
     );
 }
 
+const Main = () => {
+    return (
+        <main className="main_contact">
+            <div className="contact_container">
+                <div className="contact_top">
+                    <div className="contactTop_left">
+                        <h4>Contactez-nous</h4>
+                        <a href="">ntambube@gmail.com</a>
+                    </div>
+
+                    <div className="contactTop_right">
+                        <p>Soumettre une réclamation</p>
+                        <form action="" className="contact_form">
+                            <input type="text" name="sujet" id="sujet" placeholder="Sujet" />
+
+                            <textarea name="text" id="text" placeholder="Message"></textarea>
+                            <button type="submit">Envoyer</button>
+                        </form>
+                    </div>
+                </div>
+
+                <div className="contact_bot">
+                    <p>© 2024 Zeduc-Sp@ce. Tous les droits reservés</p>
+                </div>
+            </div>
+        </main>
+    );
+}
 
 export default Contact;
